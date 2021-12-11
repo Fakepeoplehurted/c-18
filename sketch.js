9dbaaaf1d443007186d82aad0ea96da084add17e
Fakepeoplehurted <89442138+Fakepeoplehurted@users.noreply.github.com>
2021-12-11 11:17:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var student1 
+function setup() {
+  createCanvas(400,400);
+student1=new Student ("kashni",14,9)
+student1.display()
+}
+
+function draw() 
+{
+  background(30);
+  
+  
+}
---
+++ student.js
@@ -0,0 +1,21 @@
+
+class Student
+{
+    constructor(x,age,grade)
+    {
+        this.name=x
+        this.age=age 
+        this.grade=grade 
+
+
+    }
+   display()
+   {
+       console.log(this.name)
+       console.log(this.age)
+       console.log(this.grade)
+   }
+}
+
+
+
